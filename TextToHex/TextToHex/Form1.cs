f29c5f61fc10040d761d76baba6a1f7465eb6fe4
joaonorthh <joao@northh.de>
2021-04-25 20:25:11 +0200

    Add textToHex code
    Add all project here

---
+++ TextToHex/TextToHex/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextToHex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = toHex(textBox1.Text);
+        }
+
+        public string toHex(string inp)
+        {
+            string outp = string.Empty;
+            char[] value = inp.ToCharArray();
+            foreach(char L in value)
+            {
+                int V = Convert.ToInt32(L);
+                outp += string.Format("{0:x}", V);
+            }
+            return outp;
+        }
+    }
+}
